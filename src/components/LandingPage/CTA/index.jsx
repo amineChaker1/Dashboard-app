24318bce926c3e0d82b5c44e776e987be9a4e538
Chaker Amine <chkramine2003@gmail.com>
2023-02-06 17:14:53 +0100

    'unidrive-template'

--- src/components/LandingPage/CTA/index.jsx
+++ src/components/LandingPage/CTA/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CTA = () => {
   return (
-    <div className="relative isolate overflow-hidden bg-primary px-6 pt-16 sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+    <div className="relative isolate overflow-hidden py-16 bg-primary px-6 pt-16 sm:px-16  lg:flex lg:gap-x-20 lg:px-24 ">
       <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
         <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
           Boost your studying and work with our platform
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Contact from "../components/LandingPage/Contact";
 import CTA from "../components/LandingPage/CTA";
+import Footer from "../components/LandingPage/Footer";
 import Hero from "../components/LandingPage/Hero";
 import Main from "../components/LandingPage/Main";
 
@@ -11,6 +13,8 @@ const Landing = () => {
       <Hero />
       <Main />
       <CTA />
+      <Contact />
+      <Footer />
     </>
   );
 };
